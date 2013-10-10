30695dd130b6f7ed6c56017a2f115c1a1fb78b40
MatthewJA <matthew.alger@gmail.com>
2013-10-11 10:18:03 +1100

    attempted fix of sculpture thing for safari

--- experiments/realestate/main.js
+++ experiments/realestate/main.js
@@ -3,40 +3,40 @@ var width = 800;
 var height = 500;
 
 // Grid variables
-var gridSize = 50; // 25, 20, 10 - must be common factor of width and height.
-var gridAppearance = { // Background colour for grid squares depending on their type. Replace with images later.
-	"road": "444444"
-};
+var gridSize = 50; // 50, 25, 20, 10 - should be common factor of width and height.
 
 // Arrays of things we have already, for later reference.
 var roads = [];
+var houses = [];
 
 var grid = []; // Actual grid definition.
-for (var y = 0; y < height/gridSize; y++) {
-	var p = [];
-	for (var x = 0; x < width/gridSize; x++) {
-		p.push({"contents":null, "x":x, "y":y})
-	};
-	grid.push(p);
-};
+
+// Game info
+var money = 100000;
 
 function randomInteger(min, max) {
 	return Math.floor(Math.random()*(max-min) + min);
 }
 
-function updateGridDisplay(x, y) {
-	$("#grid-" + x + "-" + y).remove();
+function makeGridDiv(x, y, w, h, type) {
+	var newDiv = $('<div/>', {
+		id: 'grid-' + x + "-" + y,
+		style: 'position:absolute; width:' + w + 'px; height:' + h + 'px;'
+	});
+	newDiv.attr('class', "viewer-" + type);
+	newDiv.css("top", y * gridSize + "px");
+	newDiv.css("left", x * gridSize + "px");
+	newDiv.appendTo('#viewer');
+}
 
-	if (grid[y][x].contents != null) {
-		var newDiv = $('<div/>', {
-			id: 'grid-' + x + "-" + y,
-			style: 'position:absolute; width:' + gridSize + 'px; height:' + gridSize + 'px; background-color:#' + gridAppearance[grid[y][x].contents]
-		});
-		newDiv.addClass("road");
-		newDiv.css("top", y * gridSize + "px");
-		newDiv.css("left", x * gridSize + "px");
-		newDiv.appendTo('#viewer');
-	}
+function updateGridDisplay(x, y) {
+	grid[y][x].reprDiv.attr('class', "viewer-" + grid[y][x].contents)
+}
+
+function addHouse(x, y) {
+	grid[y][x].contents = "house";
+	houses.push(grid[y][x]);
+	updateGridDisplay(x, y);
 }
 
 function addRoad(x, y) {
@@ -46,6 +46,16 @@ function addRoad(x, y) {
 }
 
 function initialise() {
+	// Make the grid.
+	for (var y = 0; y < height/gridSize; y++) {
+		var p = [];
+		for (var x = 0; x < width/gridSize; x++) {
+			var newDiv = makeGridDiv(x, y, gridSize, gridSize, "grass");
+			p.push({"contents":"grass", "x":x, "y":y, reprDiv:$("#grid-" + x + "-" + y)});
+		};
+		grid.push(p);
+	};
+
 	// Set the road.
 	// Road runs from a random x value less than 6, to a random length up to 10.
 	// Random y value between 4 and 7.
@@ -71,6 +81,20 @@ function initialise() {
 			addRoad(branchX, y);
 		};
 	}
+	
+	// Pick a road and add a house above it.
+	// Five times!
+	for (var i = 0; i < 5; i++) {
+		while (true) { // Is it obvious I am a Python programmer
+			var roadNum = randomInteger(0, roads.length);
+			if (roads[roadNum].y != 0) {
+				if (grid[roads[roadNum].y-1][roads[roadNum].x].contents == "grass") {
+					addHouse(roads[roadNum].x, roads[roadNum].y-1);
+					break;
+				}
+			}
+		};
+	};
 };
 
 $(document).ready(function() {
